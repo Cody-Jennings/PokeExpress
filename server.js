c91a81f90e6f5fc78c8e0729cdc40be40d736d55
Cody Jennings <cojack91@gmail.com>
2022-12-02 11:23:17 -0500

    final push

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/pokemon', (req, res) => {
 app.get('/pokemon/:indexOfPokemon', (req, res) => {
     res.render("Show", { //second param must be an object
       pokemon: pokemon[req.params.indexOfPokemon]}) 
-      // there will be a variable available inside the js file called fruit, its value is fruits[req.params.indexOfFruitsArray]
+      
   })
 
 app.listen(port, function () {
